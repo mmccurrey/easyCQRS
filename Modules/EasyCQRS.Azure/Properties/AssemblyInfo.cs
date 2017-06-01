ebdc4bf90cf681eb082795829502ee96eb339544
Kellerman Rivero <kellerman@leadbox.ca>
2017-06-01 18:18:53 -0400

    versioning .nupkg.

--- Modules/EasyCQRS.Azure/Properties/AssemblyInfo.cs
+++ Modules/EasyCQRS.Azure/Properties/AssemblyInfo.cs
@@ -10,5 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("EasyCQRS")]
 [assembly: AssemblyVersion("0.1.1.0")]
 [assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.1-alpha")]
 
 [assembly: InternalsVisibleTo("EasyCQRS.Azure.Tests")]
